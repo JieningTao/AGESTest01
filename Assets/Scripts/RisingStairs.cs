fc8bc6cd7a0487659a796c2f3e025535a5eae6d4
Jiening Tao <Jiening.Tao@iam.local>
2019-03-07 18:21:30 -0600

    optimized and class work

---
+++ Assets/Scripts/RisingStairs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingStairs : MonoBehaviour
+{
+    [SerializeField]
+    private float RisingSpeed = 5.0f;
+    [SerializeField]
+    private bool Rising = false;
+    [SerializeField]
+    Transform Stairs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Rising&&Stairs.position.y<=37)
+        {
+            
+            Stairs.transform.SetPositionAndRotation(Stairs.transform.position+new Vector3(0,RisingSpeed,0),Stairs.transform.rotation);
+
+        }
+    }
+
+
+}
