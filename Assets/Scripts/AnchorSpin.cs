3bce5a4800942a6800b0b0045096019d9f2fbe27
Jiening <Jiening.tao@loop.colum.edu>
2019-03-21 10:26:18 -0500

    finish title screen and improved some scenes

---
+++ Assets/Scripts/AnchorSpin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorSpin : MonoBehaviour
+{
+    [SerializeField]
+    private float spinSpeed;
+
+
+
+
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+    }
+}
