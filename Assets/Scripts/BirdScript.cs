b2db7d3ac0d0a94c79bb0b8661cb3b84ad411130
Jiening <Jiening.tao@loop.colum.edu>
2019-04-23 16:48:15 -0500

    transition to pond complete, need to work on crow speech system, consider using coroutines and system.sleep

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -9,17 +9,28 @@ public class BirdScript : MonoBehaviour
     [Tooltip("A reference to the crow's work panels")]
     [SerializeField]
     private GameObject WorkPanels;
+
     private string scriptLoaded;
     public static event Action<string> SaidToPlayer;
     public static event Action<string,string> CrowCommand;
     private float eventStartTime;
     private List<string> Gibberish = new List<string>();
+    private Collider proximityDetect;
+    private bool playerClose;
 
     void Awake()
     {
         scriptLoaded = SceneManager.GetActiveScene().name;
         setupGibberish();
         WorkPanels.gameObject.SetActive(false);
+        proximityDetect = GetComponent<Collider>();
+        playerClose = false;
+        eventStartTime = Time.fixedTime;
+    }
+
+    private float ScriptStarted
+    {
+        get { return Time.fixedTime - eventStartTime; }
     }
 
     // Update is called once per frame
@@ -45,70 +56,82 @@ public class BirdScript : MonoBehaviour
             case "Mumble":
                 JibberishMumbule();
                 break;
+            case "RespawnRoom2":
+                RespawnRoom2();
+                break;
+            case "LeavingFacility":
+                LeavingFacility();
+                break;
+            case "PreparingToLeaveFacility":
+                PreparingToLeaveFacility();
+                break;
+            case "JibberishMumble":
+                JibberishMumbule();
+                break;
         }
     }
 
     void FacilityUpdate()
     {
 
-        if (Time.fixedTime == 1)
+        if (ScriptStarted == 1)
         {
             WorkPanels.gameObject.SetActive(true);
             SaidToPlayer.Invoke("That looks okay.");
         } 
 
-        if (Time.fixedTime == 3)
+        if (ScriptStarted == 3)
             SaidToPlayer.Invoke("At least it doesn't look that bad.");
 
-        if (Time.fixedTime == 6.5)
+        if (ScriptStarted == 6.5)
             SaidToPlayer.Invoke("I think.");
 
-        if (Time.fixedTime == 10)
+        if (ScriptStarted == 10)
             SaidToPlayer.Invoke("And they say it was stupid complicated.");
 
-        if (Time.fixedTime == 20)
+        if (ScriptStarted == 20)
             SaidToPlayer.Invoke("That should be just about all done.");
 
-        if (Time.fixedTime == 22)
+        if (ScriptStarted == 22)
             SaidToPlayer.Invoke("Crap, forgot about the droids.");
 
     }
 
     void PondUpdate()
     {
-        if (Time.fixedTime == 1)
+        if (ScriptStarted == 1)
             SaidToPlayer.Invoke("Hi again.");
 
-        if (Time.fixedTime == 3)
+        if (ScriptStarted == 3)
             SaidToPlayer.Invoke("Welcome to the pond.");
 
-        if (Time.fixedTime == 6.5)
+        if (ScriptStarted == 6.5)
             SaidToPlayer.Invoke("This is where I come to think.");
 
-        if (Time.fixedTime == 10)
+        if (ScriptStarted == 10)
             SaidToPlayer.Invoke("I always find myself a bit more realaxed in a natural setting.");
 
-        if (Time.fixedTime == 14)
+        if (ScriptStarted == 14)
             SaidToPlayer.Invoke("Even if it is simulated.");
 
-        if (Time.fixedTime == 16)
+        if (ScriptStarted == 16)
             SaidToPlayer.Invoke("Like listening to rainstorms while you sleep.");
 
-        if (Time.fixedTime == 20)
+        if (ScriptStarted == 20)
             SaidToPlayer.Invoke("Except I hate rain.");
 
-        if (Time.fixedTime == 22)
+        if (ScriptStarted == 22)
             SaidToPlayer.Invoke("Because of the whole get wet deal.");
 
-        if (Time.fixedTime == 25)
+        if (ScriptStarted == 25)
         {
             WorkPanels.gameObject.SetActive(true);
             SaidToPlayer.Invoke("Look around for a bit, I have some backstage work to get done here.");
         }
             
-        if (Time.fixedTime == 26)
+        if (ScriptStarted == 26)
         {
-            eventStartTime = Time.fixedTime;
+            eventStartTime = ScriptStarted;
             scriptLoaded = "Mumble";
         }
             
@@ -121,33 +144,33 @@ public class BirdScript : MonoBehaviour
 
     void LightingUpdate()
     {
-        if (Time.fixedTime == 1)
+        if (ScriptStarted == 1)
             SaidToPlayer.Invoke("Hi, and welcome to...");
 
-        if (Time.fixedTime == 3)
+        if (ScriptStarted == 3)
             SaidToPlayer.Invoke("Where'd you go?");
 
-        if (Time.fixedTime == 6.5)
+        if (ScriptStarted == 6.5)
             SaidToPlayer.Invoke("Oh crap.");
 
-        if (Time.fixedTime == 10)
+        if (ScriptStarted == 10)
             SaidToPlayer.Invoke("I think I just lost you.");
 
-        if (Time.fixedTime == 14)
+        if (ScriptStarted == 14)
             SaidToPlayer.Invoke("Don't panic, just follow my voice.");
 
-        if (Time.fixedTime == 16)
+        if (ScriptStarted == 16)
             SaidToPlayer.Invoke("I knew the random spawn location thing was a pain in the tail.");
 
-        if (Time.fixedTime == 20)
+        if (ScriptStarted == 20)
             SaidToPlayer.Invoke("It's ok, i'm sure you can find me.");
 
-        if (Time.fixedTime == 22)
+        if (ScriptStarted == 22)
             SaidToPlayer.Invoke("I hope.");
 
-        if (Time.fixedTime == 26)
+        if (ScriptStarted == 26)
         {
-            eventStartTime = Time.fixedTime;
+            eventStartTime = ScriptStarted;
             scriptLoaded = "Mumble";
         }
     }
@@ -157,72 +180,168 @@ public class BirdScript : MonoBehaviour
         eventStartTime = Time.fixedTime;
         if (pointMessage == "Player met Crow")
             scriptLoaded = "FirstEncounter";
+
+        if (pointMessage == "Player entered respawn room 2")
+            scriptLoaded = "RespawnRoom2";
+
+       
     }
+
+    private void RespawnRoom2()
+    {
+        if (ScriptStarted == 0.5f)
+            SaidToPlayer.Invoke("Yeah, as you can see, i don't have a lot going on in that room yet.");
+
+        if (ScriptStarted == 3f)
+            SaidToPlayer.Invoke("In the future perhaps it will have more going on.");
+
+        if (ScriptStarted == 5f)
+            SaidToPlayer.Invoke("In any case, I need to go get some stuff.");
+
+        if (ScriptStarted == 6.5f)
+            SaidToPlayer.Invoke("You're welcome to follow me if you'd like.");
+
+        if (ScriptStarted == 8f)
+            SaidToPlayer.Invoke("Come to me when you're ready.");
+
+        if (ScriptStarted == 9f)
+            scriptLoaded = "PreparingToLeaveFacility";
+    }
+
+    private void LeavingFacility()
+    {
+        if (playerClose)
+        {
+            if (ScriptStarted == 0.5f)
+                SaidToPlayer.Invoke("All ready to go?");
+
+            if (ScriptStarted == 1.5f)
+                SaidToPlayer.Invoke("All ready to go?");
+
+            if (ScriptStarted == 2.5f)
+                SaidToPlayer.Invoke("I'll take that as a yes.");
+
+            if (ScriptStarted == 4f)
+                SaidToPlayer.Invoke("Leaving in 3...");
+
+            if (ScriptStarted == 5f)
+                SaidToPlayer.Invoke("2...");
+
+            if (ScriptStarted == 6f)
+                SaidToPlayer.Invoke("1...");
+
+            if (ScriptStarted == 7f)
+                SceneManager.LoadScene("Pond");
+        }
+        else
+        {
+            eventStartTime = Time.fixedTime;
+            scriptLoaded = "PreparingToLeaveFacility";
+        }
+
+        
+    }
+
+    private void PreparingToLeaveFacility()
+    {
+        if (!playerClose)
+        {
+            JibberishMumbule();
+        }
+        else
+        {
+            eventStartTime = Time.fixedTime;
+            scriptLoaded = "LeavingFacility";
+        }
+    }
+
 
     private void FoundCrowInLighting()
     {
-        if (Time.fixedTime - eventStartTime == 0.5f)
+        if (ScriptStarted == 0.5f)
             SaidToPlayer.Invoke("Oh there you are.");
     }
 
     private void FirstEncounter()
     {
 
-        if (Time.fixedTime - eventStartTime == 1f)
+        if (ScriptStarted == 1f)
         {
             SaidToPlayer.Invoke("Oh...         ");
             WorkPanels.gameObject.SetActive(false);
         }
             
-        if (Time.fixedTime - eventStartTime == 4)
+        if (ScriptStarted == 4)
             SaidToPlayer.Invoke("You...");
 
-        if (Time.fixedTime - eventStartTime == 6)
+        if (ScriptStarted == 6)
             SaidToPlayer.Invoke("Are you a player?");
 
-        if (Time.fixedTime - eventStartTime == 8)
+        if (ScriptStarted == 8)
             SaidToPlayer.Invoke("You're not supposed to be here.");
 
-        if (Time.fixedTime - eventStartTime == 11)
+        if (ScriptStarted == 11)
             SaidToPlayer.Invoke("The game's not done.");
 
-        if (Time.fixedTime - eventStartTime == 13)
+        if (ScriptStarted == 13)
             SaidToPlayer.Invoke("It's not ready.");
 
-        if (Time.fixedTime - eventStartTime == 15)
+        if (ScriptStarted == 15)
             SaidToPlayer.Invoke("How did you even get here?");
 
-        if (Time.fixedTime - eventStartTime == 17)
+        if (ScriptStarted == 17)
             SaidToPlayer.Invoke("Can you speak?");
 
-        if (Time.fixedTime - eventStartTime == 18.5)
+        if (ScriptStarted == 18.5)
             SaidToPlayer.Invoke("Try typing.");
 
-        if (Time.fixedTime - eventStartTime == 21)
+        if (ScriptStarted == 21)
             SaidToPlayer.Invoke("Oh yeah, I didn't Implement the speech system yet.");
 
-        if (Time.fixedTime - eventStartTime == 24)
+        if (ScriptStarted == 24)
             SaidToPlayer.Invoke("Well the game isn't finished so...");
 
-        if (Time.fixedTime - eventStartTime == 26)
+        if (ScriptStarted == 26)
             SaidToPlayer.Invoke("I mean, I'm not gonna ask you to leave.");
 
-        if (Time.fixedTime - eventStartTime == 28)
+        if (ScriptStarted == 28)
             SaidToPlayer.Invoke("Feel free to take a look around I guess.");
 
-        if (Time.fixedTime - eventStartTime == 30)
+        if (ScriptStarted == 30)
             SaidToPlayer.Invoke("I tend to mumble a bit to myself a bit when i work, don't mind me.");
 
-        //Debug.Log(Time.fixedTime - eventStartTime);
+        if (ScriptStarted == 32)
+        {
+            WorkPanels.gameObject.SetActive(true);
+            scriptLoaded = "JibberishMumble";
+        }
+        //Debug.Log(ScriptStarted);
     }
 
     private void JibberishMumbule()
     {
-        if ((Time.fixedTime - eventStartTime) % 6 == 0)
+        if ((ScriptStarted) % 6 == 0)
         {
             SaidToPlayer.Invoke( Gibberish[UnityEngine.Random.Range(0, Gibberish.Count - 1)]);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerClose = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerClose = false;
+        }
+    }
+
     private void setupGibberish()
     {
         Gibberish.Add("Hummmm......");
